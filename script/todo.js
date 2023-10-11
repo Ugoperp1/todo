3edba288ffee38c2060cd14e7ce10eed324c8b54
Ugoperp1 <Ugoperpetua2@gmail.com>
2023-10-11 15:02:35 +0100

    1st commit

---
+++ script/todo.js
@@ -0,0 +1,48 @@
+var enterButton = document.getElementById("enter");
+var input = document.getElementById("userInput");
+var ul = document.querySelector("ul");
+var item = document.getElementsByTagName("li");
+
+
+function inputLength() {
+    return input.value.length
+}
+
+function createListElement() {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+
+    function crossOut() {
+        li.classList.toggle("done")
+    }
+
+    li.addEventListener("click", crossOut)
+
+    var deleteButton = document.createElement("button")
+    deleteButton.appendChild(document.createTextNode("X"))
+    li.appendChild(deleteButton)
+
+    function deleteListItem() {
+        li.classList.add("delete")
+    }
+
+    deleteButton.addEventListener("click", deleteListItem)
+}
+
+function addListAfterClick() {
+    if (inputLength() > 0) {
+        createListElement()
+    }
+
+}
+function addListAfterKeypress(event){
+    if (inputLength() > 0 && event.which === 13){
+        createListElement()
+    }
+}
+// enterButton.addEventListener("click", addListAfterClick())
+
+enterButton.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress);
